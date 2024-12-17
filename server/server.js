1a7d6612ee9f9d47fd7c9dc37999692cffd74377
Mayank <134766594+vortex-m@users.noreply.github.com>
2024-12-17 13:31:13 +0530

    Update Server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
-const express = require('express');
+import dotenv from "dotenv";
+import connectDb from "./config/connectdb.js";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+
+const DATABASE_URL = process.env.DATABASE_URL;
+connectDb(DATABASE_URL);
+
 const app = express();
+const port = process.env.PORT || 3000;
 
-const PORT = 3000;
+app.use(cors());
 
-app.get('/',(req,res)=>{
-   res.send('Hello World');
-})
-
-app.listen(PORT,()=>{
-   console.log(`Server is running on port ${PORT}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
