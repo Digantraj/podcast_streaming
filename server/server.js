739368ceb9306abfe134b17c1bdab87f70cf3ef7
Mayank <134766594+vortex-m@users.noreply.github.com>
2024-12-20 02:20:49 +0530

    Major Updates

--- server/server.js
+++ server/server.js
@@ -1,18 +1,35 @@
-import dotenv from "dotenv";
-import connectDb from "./config/connectdb.js";
-dotenv.config();
-
-import express from "express";
-import cors from "cors";
-
-const DATABASE_URL = process.env.DATABASE_URL;
-connectDb(DATABASE_URL);
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.use(cors());
+const connectDb = require("./config/db");
+const userApi = require("./routes/user");
+const catApi = require("./routes/categories.js");
+const podcastApi = require("./routes/podcast.js");
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.use(express.json());
+app.use(cookieParser());
+require("dotenv").config();
+
+// CORS
+app.use(
+  cors({
+    origin: "http://localhost:5173", 
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    credentials: true,
+  })
+);
+
+// Connect to Database
+connectDb();
+
+// All routes
+app.use("/api/v1", userApi);
+app.use("/api/v1", catApi);
+app.use("/api/v1", podcastApi);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
